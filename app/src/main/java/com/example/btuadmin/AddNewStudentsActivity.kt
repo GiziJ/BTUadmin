ba1ea75ea653011076a330a874ed11c1c7136fac
GiziJaliashvili <gizi.jaliashvili.1@btu.edu.ge>
2022-01-05 22:04:47 +0400

    BTUadmin SaveButton davamate :D

--- app/src/main/java/com/example/btuadmin/AddNewStudentsActivity.kt
+++ app/src/main/java/com/example/btuadmin/AddNewStudentsActivity.kt
@@ -1,7 +1,8 @@
 package com.example.btuadmin
 
+import android.content.Intent
 import android.os.Bundle
-import android.os.PersistableBundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.btuadmin.databinding.ActivityAddNewStudentsBinding
 
@@ -13,12 +14,32 @@ class AddNewStudentsActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityAddNewStudentsBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        init()
+
+        saveInfo()
+
     }
-
-    private fun init(){
+    //ახალი სტუდენტსი დამატება
+    private fun saveInfo(){
         binding.buttonSave.setOnClickListener{
+            val studentID = binding.editTextID.text.toString()
+            val studentName = binding.editTextStudentsName.text.toString()
+            val studentsEmail = binding.editTextStudentsEmail.text.toString()
+            val studentDate = binding.editTextDate.text.toString()
+            val studentAddress = binding.editTextLegalAddress.text.toString()
 
+            if (studentID.isEmpty() || studentName.isEmpty() || studentsEmail.isEmpty() || studentDate.isEmpty() || studentAddress.isEmpty()){
+                Toast.makeText(this, "შეავსეთ მონაცემები", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }else{
+                Toast.makeText(this, "ახალი სტუენტი დამატებულია", Toast.LENGTH_SHORT).show()
+                binding.editTextID.text.clear()
+                binding.editTextStudentsName.text.clear()
+                binding.editTextStudentsEmail.text.clear()
+                binding.editTextDate.text.clear()
+                binding.editTextLegalAddress.text.clear()
+                startActivity(Intent(this, ExistingStudentsListActivity::class.java))
+                return@setOnClickListener
+            }
         }
     }
 }
